be4f7a54fa6db8602d186b265d4a8cc96f79e8f9
zombie035 <167063563+zombie035@users.noreply.github.com>
2025-12-26 09:56:01 +0530

    Update api.js

--- data/api.js
+++ data/api.js
@@ -1,5 +1,9 @@
-import users from "../data/users.json";
+import users from "./users.json";
 
-export default function handler(req, res) {
+export default function (req, res) {
+  if (req.method !== "GET") {
+    return res.status(403).json({ error: "Read only API" });
+  }
+
   res.status(200).json(users);
 }
